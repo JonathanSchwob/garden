ac49665a699798e432765441c4c478423f783958
Jonathan Schwob <jrschwob@gmail.com>
2023-10-06 01:51:12 -0700

    render images and names

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
-async function PlantCount() {
+import Image from "next/image";
+
+async function plantSearch() {
   const response = await fetch(
-    "https://trefle.io/api/v1/plants?token=jSujFE501ZbE3cqPJwAnniHbVLBnikORbrpj_r8JG0c"
+    "https://trefle.io/api/v1/plants/search?token=jSujFE501ZbE3cqPJwAnniHbVLBnikORbrpj_r8JG0c&q=rose"
   );
   const body = await response.json();
-  console.log(body, "start");
-  const totalPlants = body.data.reduce(
-    // @ts-expect-error
-    (acc, item) => acc + item.plant_count,
-    0
-  );
-  return <div>{totalPlants.toLocaleString()} total plants</div>;
+  console.log(body.data, "start");
+  return body.data;
 }
-console.log("wtf");
-export default function Home() {
+
+export default async function Home() {
+  const plants = await plantSearch();
   return (
     <main className="min-h screen p-8 flex felx-col gap-y-8">
       <div className="grid grid-cols-4 ga-2 justify-items-center"></div>
-      <PlantCount />
+      {plants.map((plant: any, key: any) => (
+        <div>
+          <Image
+            key={key}
+            src={plant.image_url}
+            width={50 * 1.5}
+            height={50 * 1.5}
+            alt={plant.common_name}
+          />
+          <div>{plant.common_name}</div>
+        </div>
+      ))}
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bs.plantnet.org",
+        port: "",
+        pathname: "/image/o/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
